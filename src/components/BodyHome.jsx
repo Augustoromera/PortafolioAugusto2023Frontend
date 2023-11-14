d1be8b580257a65233693768622e3375a4e36b3d
Augusto <newsromers@gmail.com>
2023-11-13 22:52:23 -0300

    se instalo react boostrap, se agrego una carpeta image, profile, y una styles con un archivo css para bodyhome.jsx y una presentacion con nombre y perfil laboral

--- src/components/BodyHome.jsx
+++ src/components/BodyHome.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
-
+import profileImage from '../assets/images/profile/profile-augusto.png'
+import '../styles/bodyhome.css';
 const BodyHome = () => {
     return (
-        <div>
-            <h1>Hola Bienvenido al portafolio de augusto</h1>
+        <div className='container'>
+            <div className='row descrip-container'>
+                <img src={profileImage} className='profile-image' alt="Profile Augusto" />
+                <div className="col">
+                    <h1 className="text-center text-white" style={{  fontWeight:"100"}}>Hola!</h1>
+                    <h1 className="text-center text-primary font-weight-bold" style={{ fontSize: "2.2em" , fontWeight:"600"}}>Soy Augusto Romera</h1>
+                    <h1 className="text-center text-secondary" style={{ fontSize: "1.5em" }}>Fullstack Development</h1>
+
+                </div>
+            </div>
         </div>
     );
 };

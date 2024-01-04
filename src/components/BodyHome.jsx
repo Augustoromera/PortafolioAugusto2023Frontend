4bf6dc9496fbee753cd34da42bcff83319cd67c3
Augusto <newsromers@gmail.com>
2024-01-04 16:02:29 -0300

    Se agrego un boton para descargar curriculum en la seccion 'Sobre mi', Se modifico el comportamiento de los portafolios al hacerle hover, se implemento logica para cambiar dinamicamente el color de los iconos al hacer hover en 'Contact me', se agregaron variables en css para los colores

--- src/components/BodyHome.jsx
+++ src/components/BodyHome.jsx
@@ -6,6 +6,8 @@ import nodeLogo from '../assets/images/logo/node.png';
 import expressLogo from '../assets/images/logo/express.png';
 import mongoDBLogo from '../assets/images/logo/Mongo.png';
 import rapiburger from '../assets/images/portfolio/Rapiburger.png';
+import instagram from '../assets/images/logo/Instagram-Glyph-Color-Logo.wine.png';
+import curriculumPDF from '../assets/files/Curriculum Vitae augusto.pdf';
 import ScrollReveal from 'scrollreveal';
 const ProfileImage = () => {
     return <img src={profileImage} className='profile-image' alt="Profile Augusto" />;
@@ -18,17 +20,17 @@ const BodyHome = () => {
         { id: 3, name: 'En proceso', image: rapiburger, website: 'https://proyecto3.com', github: 'https://github.com/proyecto3', text: 'Ir a proyecto 3' },
         { id: 4, name: 'En proceso', image: rapiburger, website: 'https://proyecto4.com', github: 'https://github.com/proyecto4', text: 'Ir a proyecto 4' }
     ];
-    const handleHover = (projectId, projectName) => {
+    const handleHover = (projectId) => {
         const projectText = document.getElementById(`projectText-${projectId}`);
         const linksContainer = document.createElement('div');
 
         const websiteLink = document.createElement('a');
         websiteLink.href = projects.find(project => project.id === projectId).website;
         websiteLink.textContent = projects.find(project => project.id === projectId).text;
-        websiteLink.classList.add('project-link'); 
+        websiteLink.classList.add('project-link');
         linksContainer.appendChild(websiteLink);
         websiteLink.target = '_blank';
-        
+
         linksContainer.appendChild(document.createTextNode(' | '));
 
         const githubLink = document.createElement('a');
@@ -40,6 +42,13 @@ const BodyHome = () => {
         projectText.innerHTML = '';
         projectText.appendChild(linksContainer);
         projectText.classList.add('mt-2-show');
+        projectText.style.transition = 'opacity 0.7s';
+    };
+    const handleMouseLeave = (projectId) => {
+        const projectText = document.getElementById(`projectText-${projectId}`);
+        projectText.style.transition = '';
+        projectText.innerHTML = '';
+        projectText.classList.remove('mt-2-show');
     };
 
     useEffect(() => {
@@ -47,7 +56,6 @@ const BodyHome = () => {
         ScrollReveal().reveal('.about_skills', { delay: 300 });
         ScrollReveal().reveal('.portfolio', { delay: 300 });
         ScrollReveal().reveal('.contact-me', { delay: 300 });
-        ScrollReveal().reveal('.container', { delay: 300 });
         ScrollReveal().reveal('.col', { delay: 300 });
     }, []);
 
@@ -72,6 +80,11 @@ const BodyHome = () => {
                     <p className='description'>
                         Soy Augusto Romera, estudiante de Ingeniería en Sistemas con habilidades en desarrollo web. Tengo experiencia en colaboración y trabajo en equipo, y actualmente estoy buscando una oportunidad de tiempo completo en el campo del desarrollo web. Me apasiona aplicar mis conocimientos y habilidades para seguir mejorando y contribuir al éxito del equipo.
                     </p>
+                    {/* Botón de Descargar CV con ícono de descarga */}
+                    <a href={curriculumPDF} download className="download-button">
+                        <i className="fas fa-download download-icon"></i>
+                        <span className="download-text"> Descargar CV</span>
+                    </a>
                 </div>
                 <div className="about_skills">
                     <div className="about_skill_title">
@@ -124,7 +137,8 @@ const BodyHome = () => {
                                 key={project.id}
                                 className="col-12 col-sm-6 col-md-4 p-4 d-flex align-items-center flex-column mt-2-container"
                                 onMouseEnter={() => handleHover(project.id, project.name)}
-                                >
+                                onMouseLeave={() => handleMouseLeave(project.id)}
+                            >
                                 <img src={project.image} className="image-portfolio" alt={project.name} />
                                 <span id={`projectText-${project.id}`} className="mt-2"></span>
                             </div>
@@ -133,38 +147,43 @@ const BodyHome = () => {
                     </div>
                 </div>
             </div >
-            {/* Sección de Contacto */}
-            < div className="contact-me" >
+            <div className="contact-me">
                 <div className="contact-me-text px-3">
                     <h2>Contacto</h2>
                     <p>Contáctame si quieres que trabajemos juntos.</p>
                 </div>
 
-                <div className="container">
+                <div className="container contact-me-container">
                     <div className="row justify-content-center">
-                        {/* Gmail */}
                         <div className="col-6 col-md-3 p-2 d-flex align-items-center flex-column">
-                            <img src={nodeLogo} className="contact-icon" alt="Gmail" />
-                            <span className="mt-2">Escribir</span>
+                            <a href="mailto:newsromers@gmail.com" className="plain-link">
+                                <i className="far fa-envelope fa-3x text-danger iconlink"></i>
+                            </a>
+                            <span className="textcontact">Escribir</span>
                         </div>
-                        {/* GitHub */}
+
                         <div className="col-6 col-md-3 p-2 d-flex align-items-center flex-column">
-                            <img src={nodeLogo} className="contact-icon" alt="GitHub" />
-                            <span className="mt-2">Ver Repositorio</span>
+                            <a href="https://github.com/Augustoromera" className="plain-link" target="_blank" rel="noopener noreferrer">
+                                <i className="fab fa-github fa-3x text-dark iconlink"></i>
+                            </a>
+                            <span className="textcontact">Ver Repo</span>
                         </div>
-                        {/* Instagram */}
                         <div className="col-6 col-md-3 p-2 d-flex align-items-center flex-column">
-                            <img src={nodeLogo} className="contact-icon" alt="Instagram" />
-                            <span className="mt-2">Seguir</span>
+                            <a href="https://www.instagram.com/augusto.romera/" target="_blank" rel="noopener noreferrer">
+                                <img src={instagram} alt="logo de instagram" className='contact-icon' style={{ maxWidth: '73px', height: '100%' }} />
+                            </a>
+                            <span className="textcontact">Seguir</span>
                         </div>
-                        {/* LinkedIn */}
                         <div className="col-6 col-md-3 p-2 d-flex align-items-center flex-column">
-                            <img src={nodeLogo} className="contact-icon" alt="LinkedIn" />
-                            <span className="mt-2">Contactar</span>
+                            <a href="https:/www.linkedin.com/in/augusto-romera" className="plain-link" target="_blank" rel="noopener noreferrer">
+                                <i className="fab fa-linkedin-in fa-3x text-primary iconlink"></i>
+                            </a>
+                            <span className="textcontact">Contactar</span>
                         </div>
                     </div>
                 </div>
-            </div >
+
+            </div>
             <div className="copyright pt-5">
                 <p className="text-center">
                     Copyright © 2023. @augusto.romera. All Rights Reserved.
